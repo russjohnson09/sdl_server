6e7b4b2219ada7399cbb2f5e177731e3b6f9cdcc
Russell Johnson <russell.johnson@livio.io>
2019-05-31 16:25:23 -0400

    update nginx

--- devops/deploy_scripts/start_pm2.sh
+++ devops/deploy_scripts/start_pm2.sh
@@ -10,4 +10,8 @@ echo $dir
 
 
 #create link to the pm2 that you want to actually run against
+#sudo ln -s "devops/deploy_scripts/pm2/glc-oem.pm2.config.js" "devops/deploy_scripts/pm2/active.pm2.config.js" || true
+#sudo ln -s /home/ubuntu/sdl_policy_servers/glc-oem/devops/deploy_scripts/pm2/glc-oem.pm2.config.js /home/ubuntu/sdl_policy_servers/glc-oem/devops/deploy_scripts/pm2/active.pm2.config.js
+#sudo ln -s /home/ubuntu/sdl_policy_servers/russjohnson09-oem/devops/deploy_scripts/pm2/russjohnson09-oem.pm2.config.js /home/ubuntu/sdl_policy_servers/glc-oem/devops/deploy_scripts/pm2/active.pm2.config.js
+
 pm2 startOrGracefulReload devops/deploy_scripts/pm2/active.pm2.config.js
