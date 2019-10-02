4fa640b6444346817c7f43711f433d8824ed0d06
Russell Johnson <russell.johnson@livio.io>
2019-10-02 13:47:47 -0400

    init get

--- app/v1/vehicle-data/model.js
+++ app/v1/vehicle-data/model.js
@@ -1,5 +1,14 @@
 //Copyright (c) 2019, Livio, Inc.
 const app = require('../app');
 
+
+function getVehicleData(isProduction, cb)
+{
+
+}
+
 module.exports = {
+
+
+    getVehicleData: getVehicleData,
 };
